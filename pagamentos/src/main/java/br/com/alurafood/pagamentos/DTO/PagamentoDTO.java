608c6a1ffb06dfe5e3b377c759f05de35b708de1
andmaia <andrewrian198@gmail.com>
2023-05-22 23:09:41 -0300

    Adicionando camada controller na aplicação

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/DTO/PagamentoDTO.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/DTO/PagamentoDTO.java
@@ -1,6 +1,15 @@
 package br.com.alurafood.pagamentos.DTO;
 
-public record PagamentoDTO(
+import br.com.alurafood.pagamentos.model.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
+import java.math.BigDecimal;
+
+public record PagamentoDTO(Long id,BigDecimal valor,String nome,String numero,String expiracao,String codigo,Status status,Long pedidoId,
+        Long formaDePagamentoId
 ) {
 }
